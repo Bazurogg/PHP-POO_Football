ab91a9321fd95074fee8a43dc5f16ec95240fdb0
Bazurogg <bazurogg@gmail.com>
2023-06-26 09:44:47 +0200

    Setuping class / Pays -> Equipe

--- ClassFootball/Equipe.php
+++ ClassFootball/Equipe.php
@@ -2,7 +2,29 @@
 
 class Equipe {
 
+    private Pays $_pays;
     private string $_nomEquipe;
     private string $_dateCreation;
-    
+    private array $_contrat;
+
+    public function __construct(Pays $pays, $nomEquipe, $dateCreation) {
+
+        $pays -> ajouterEquipe($this);
+        $this -> _pays = $pays;
+        $this -> _dateCreation = $dateCreation;
+        $this -> _contrat = [];
+        
+    }
+
+    public function setNomEquipe($nomEquipe) {
+        
+        return $this -> _nomEquipe = $nomEquipe;
+
+    }
+
+    public function setDateCreattion($dateCreation) {
+
+        return $this -> _dateCreation = $dateCreation;
+        
+    }
 }
--- ClassFootball/Pays.php
+++ ClassFootball/Pays.php
@@ -4,8 +4,42 @@ class Pays {
 
     private string $_nomPays;
     private array $_equipes;
-    private array $_joueurs;
 
+ 
+    public function __construct($nomPays, $_dateCreation) {
+   
+        $this -> _nomPays = $nomPays;
+        $this -> _equipes = [];
+
+    }
     
+    public function setNomPays($nomPays) {
 
+        $this -> _nomPays = $nomPays;
+
+    }
+
+    public function getNomPays() {
+        
+        return $this -> _nomPays;
+    
+    }
+    
+    public function afficherEquipe() {
+        foreach ($this -> _equipes as $equipe) {
+            echo $equipe; 
+        }
+    }
+
+    public function ajouterEquipe(Equipe $equipe) {
+
+        $this -> _equipes [] = $equipe;
+
+    }
+
+    public function __toString() {
+
+        return "<span>" . $this -> getNomPays() . "</span><br>";
+        
+    }
 }
