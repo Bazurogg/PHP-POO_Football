175b614b12de4ed33bbf3d7fa2b39b41513e5a9e
Bazurogg <bazurogg@gmail.com>
2023-06-27 21:31:50 +0200

    Adding Backup files for Class Joueur / retake for the bi-citizen player  attribute

--- Backup_files/Joueur.php
+++ Backup_files/Joueur.php
@@ -5,14 +5,20 @@
         private string $_nom;
         private string $_prenom;
         private string $_dateNaissance;
+        private Pays $_pays1;
+        private ?Pays $_pays2; // "?" Indique qu'il peut contenir une instance de la class Pays OU null. Sinon erreur PHP -> "Fatal error: Uncaught TypeError: Cannot assign null to property"
         private array $_mercato;
         
         
-        public function __construct($prenom, $nom, $dateNaissance){
+        public function __construct($prenom, $nom, $dateNaissance, Pays $pays1, Pays $pays2 = null){
             
             $this -> _prenom = $prenom;
             $this -> _nom = $nom;
             $this -> _dateNaissance = $dateNaissance;
+            $pays1 -> ajouterJoueur($this);
+            if ($pays2) {
+                $pays2->ajouterJoueur($this);
+            }
             $this -> _mercato = [];
             
         }
@@ -46,6 +52,16 @@
     
         }
     
+        public function getPays() {
+
+            return $this->_pays1;
+
+        }
+
+        public function getPays2() {
+            return $this->_pays2;
+        }
+        
         public function getDateNaissance() {
     
             return $this -> _dateNaissance;
@@ -71,6 +87,16 @@
         public function afficherRecapJoueur() {
 
             echo "<strong>" . $this -> getPrenom() . " " . $this -> getNom() . "</strong><br>" . $this -> getAge() . " ans. <br>";
+
+            echo $this -> getPays() -> getNomPays();
+
+            if ($this->getPays2()) {
+
+                echo "/" . $this -> getPays2() -> getNomPays();
+
+            }
+
+            echo "<br>";
 
             foreach ($this -> _mercato as $mercato) {
                 
--- index.php
+++ index.php
@@ -22,15 +22,15 @@ $equipe07 = new Equipe($pays01, "OM", "1899");
 $equipe08 = new Equipe($pays06, "Bayern Munich", "1900");
 
 
-$joueur01 = new Joueur("Kylian", "Mbappe", "20-12-1998", $pays01);     
+$joueur01 = new Joueur("Kylian", "Mbappe", "20-12-1998");     
 // $joueur02 = new Joueur("Christiano", "Ronaldo", "5-02-1985"); 
-$joueur03 = new Joueur("Lionel", "Messi", "24-06-1987", $pays05);
+$joueur03 = new Joueur("Lionel", "Messi", "24-06-1987");
 // $joueur04 = new Joueur("Neymar", "da Silva Santos", "2-02-1992" );
-$joueur05 = new Joueur("Dimitri", "Payet", "29-03-1987", $pays01);
-$joueur06 = new Joueur("Lucas", "Hernandez", "14-02-1996", $pays01, $pays02);
+$joueur05 = new Joueur("Dimitri", "Payet", "29-03-1987");
+$joueur06 = new Joueur("Lucas", "Hernandez", "14-02-1996");
 
-$nationalite01 = new Nationalite($joueur01, $pays01);
-$nationalite02 = new Nationalite($joueur01, $pays05);
+$nationalite01 = new Nationalite($joueur06, $pays01);
+$nationalite02 = new Nationalite($joueur06, $pays02);
 
 $transfert01 = new Mercato($joueur01, $equipe01, "2017");
 $transfert02 = new Mercato($joueur03, $equipe01, "2021");
